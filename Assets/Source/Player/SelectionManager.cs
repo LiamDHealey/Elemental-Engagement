1ce6c9276369ff40ec8671ae4d3cb18aa259e8e8
Liam Healey <56327428+LiamDHealey@users.noreply.github.com>
2023-09-08 16:34:46 -0600

    Selection

--- Assets/Source/Player/SelectionManager.cs
+++ Assets/Source/Player/SelectionManager.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
-public class SelectionManager : MonoBehaviour
+namespace ElementalEngagement.Player
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// Handles the selection of selectable objects. Can select either by clicking on an individual selectable 
+    /// or by clicking and dragging to create a selection box which will select all members of the selection group with the most member within the box.
+    /// </summary>
+    [RequireComponent(typeof(Allegiance))]
+    public class SelectionManager : MonoBehaviour
     {
-        
-    }
+        [Tooltip("The input component that this will get mouse data from.")]
+        [SerializeField] private PlayerInput input;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [Tooltip("The camera from which to make the selection.")]
+        [SerializeField] private new Camera camera;
+
+        [Tooltip("The camera from which to make the selection.")]
+        [SerializeField] private Canvas hudCanvas;
+
+        [Tooltip("The camera from which to make the selection.")]
+        [SerializeField] private RectTransform selectionBox;
+
+        [Tooltip("The minimum distance from initial click location to create a selection box for.")]
+        [SerializeField] private float minBoxSize = 10;
     }
 }
