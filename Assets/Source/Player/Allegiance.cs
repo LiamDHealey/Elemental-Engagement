48fa8b53fc4d780e24fe37c8a8ed014596c733e8
Devin Fink <devinfink007@gmail.com>
2023-09-20 20:39:20 -0600

    Added Helper Methods to Allegiance
    One helper method checks for any alignment, whereas the other method checks for dual alignment

--- Assets/Source/Player/Allegiance.cs
+++ Assets/Source/Player/Allegiance.cs
@@ -1,6 +1,4 @@
 using ElementalEngagement.Favor;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace ElementalEngagement.Player
@@ -15,5 +13,31 @@ namespace ElementalEngagement.Player
 
         // The god this is aligned with.
         public MinorGod god;
+
+        /// <summary>
+        /// Returns true if the two units are aligned with either faction or God
+        /// </summary>
+        /// <param name="selfAllegiance"></param>
+        /// <param name="targetAllegiance"></param>
+        /// <returns>true if aligned, false otherwise</returns>
+        public static bool CheckAnyAllegiance(Allegiance selfAllegiance, Allegiance targetAllegiance)
+        {
+            return (selfAllegiance != null && targetAllegiance != null &&
+                (selfAllegiance.faction != Faction.Unaligned && selfAllegiance.faction == targetAllegiance.faction ||
+                selfAllegiance.god != Favor.MinorGod.Unaligned && selfAllegiance.god == targetAllegiance.god));
+        }
+
+        /// <summary>
+        /// Returns true if the two units are aligned with both faction AND God
+        /// </summary>
+        /// <param name="selfAllegiance"></param>
+        /// <param name="targetAllegiance"></param>
+        /// <returns>true if aligned, false if not</returns>
+        public static bool CheckBothAllegiance(Allegiance selfAllegiance, Allegiance targetAllegiance)
+        {
+            return (selfAllegiance != null && targetAllegiance != null &&
+                (selfAllegiance.faction != Faction.Unaligned && selfAllegiance.faction == targetAllegiance.faction &&
+                selfAllegiance.god != Favor.MinorGod.Unaligned && selfAllegiance.god == targetAllegiance.god));
+        }
     }
 }
