4c0347e9e8b821ff679c89a736152acc7123113a
Liam Healey <56327428+LiamDHealey@users.noreply.github.com>
2023-09-21 11:40:14 -0600

    Add unit animations

--- Assets/Source/Utilities/AnimatorFunctionality.cs
+++ Assets/Source/Utilities/AnimatorFunctionality.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace ElementalEngagement.Utilities
 {
@@ -11,12 +12,28 @@ namespace ElementalEngagement.Utilities
     /// </summary>
     public class AnimatorFunctionality : MonoBehaviour
     {
+        [Tooltip("The animator this is helping.")]
+        [SerializeField] private Animator animator;
+
+        [Tooltip("The name of the animator parameter used to check if the animator is moving.")]
+        [SerializeField] private string movingParameterName = "IsMoving";
+
+        [Tooltip("The minimum distance this needs to move to be consider moving.")] [Min(0)]
+        public float moveThreshed = 1.0f;
+
+        // Whether or not this is currently moving
+        public bool isMoving { get; private set; } = false;
+
+        // The last position this was at.
+        private Vector3 lastPosition;
+
+
         /// <summary>
         /// Destroys this component's game object
         /// </summary>
         public void DestroySelf()
         {
-            throw new System.NotImplementedException();
+            Destroy(gameObject);
         }
 
         /// <summary>
@@ -24,7 +41,30 @@ namespace ElementalEngagement.Utilities
         /// </summary>
         public void DestroyParent()
         {
-            throw new System.NotImplementedException();
+            Destroy(transform.parent.gameObject);
+        }
+
+        /// <summary>
+        /// Initialized last position.
+        /// </summary>
+        private void Start()
+        {
+            lastPosition = transform.position;
+        }
+
+        /// <summary>
+        /// Updates moving state
+        /// </summary>
+        public void FixedUpdate()
+        {
+            bool moved = (lastPosition - transform.position).sqrMagnitude/Time.fixedDeltaTime > moveThreshed * moveThreshed;
+            
+            if (moved != isMoving)
+            {
+                isMoving = moved;
+                GetComponent<Animator>().SetBool(movingParameterName, isMoving);
+            }
+            lastPosition  = transform.position;
         }
     }
 }
