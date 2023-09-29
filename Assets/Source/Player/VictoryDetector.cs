caf236ac499bb8d63459befacdef2721225264f5
Liam Healey <56327428+LiamDHealey@users.noreply.github.com>
2023-09-29 15:47:17 -0600

    Start on death screen

--- Assets/Source/Player/VictoryDetector.cs
+++ Assets/Source/Player/VictoryDetector.cs
@@ -7,51 +7,54 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class VictoryDetector : MonoBehaviour
+namespace ElementalEngagement.Player
 {
-    [Tooltip("The players and the structures they need to keep alive to stay in the game.")]
-    [SerializeField] private List<PlayerToVitalStructures> playersToVitalStructures;
-
-    [Tooltip("Called when a player has lost.")]
-    public UnityEvent<Faction> onPlayerLost;
-
-    // The factions map to their loose conditions.
-    public ReadOnlyDictionary<Faction, ReadOnlyCollection<Health>> factionToVitalStructures;
-
-    private void Start()
+    public class VictoryDetector : MonoBehaviour
     {
-        factionToVitalStructures = new ReadOnlyDictionary<Faction, ReadOnlyCollection<Health>>(
-            playersToVitalStructures.ToDictionary(ptvs => ptvs.faction, ptvs => ptvs.vitalStructures.AsReadOnly()));
+        [Tooltip("The players and the structures they need to keep alive to stay in the game.")]
+        [SerializeField] private List<PlayerToVitalStructures> playersToVitalStructures;
 
-        foreach (PlayerToVitalStructures playerToVitalStructures in playersToVitalStructures)
+        [Tooltip("Called when a player has lost.")]
+        public UnityEvent<Faction> onPlayerLost;
+
+        // The factions map to their loose conditions.
+        public ReadOnlyDictionary<Faction, ReadOnlyCollection<Health>> factionToVitalStructures;
+
+        private void Start()
         {
-            foreach (Health vitalStructure in  playerToVitalStructures.vitalStructures)
-            {
-                vitalStructure.onKilled.AddListener(CheckForLosses);
-                void CheckForLosses()
-                {
-                    // See if any other vital structure for this faction are still alive
-                    foreach (Health otherVitalStructure in playerToVitalStructures.vitalStructures)
-                    {
-                        if (otherVitalStructure != null)
-                            return;
-                    }
+            factionToVitalStructures = new ReadOnlyDictionary<Faction, ReadOnlyCollection<Health>>(
+                playersToVitalStructures.ToDictionary(ptvs => ptvs.faction, ptvs => ptvs.vitalStructures.AsReadOnly()));
 
-                    onPlayerLost?.Invoke(playerToVitalStructures.faction);
+            foreach (PlayerToVitalStructures playerToVitalStructures in playersToVitalStructures)
+            {
+                foreach (Health vitalStructure in playerToVitalStructures.vitalStructures)
+                {
+                    vitalStructure.onKilled.AddListener(CheckForLosses);
+                    void CheckForLosses()
+                    {
+                        // See if any other vital structure for this faction are still alive
+                        foreach (Health otherVitalStructure in playerToVitalStructures.vitalStructures)
+                        {
+                            if (otherVitalStructure != null)
+                                return;
+                        }
+
+                        onPlayerLost?.Invoke(playerToVitalStructures.faction);
+                    }
                 }
             }
         }
-    }
 
-    /// <summary>
-    /// Class for mapping factions to vital structures.
-    /// </summary>
-    private class PlayerToVitalStructures
-    {
-        [Tooltip("The faction with who's vital structures are listed.")]
-        public Faction faction;
+        /// <summary>
+        /// Class for mapping factions to vital structures.
+        /// </summary>
+        private class PlayerToVitalStructures
+        {
+            [Tooltip("The faction with who's vital structures are listed.")]
+            public Faction faction;
 
-        [Tooltip("The structures that the faction must not let be killed.")]
-        public List<Health> vitalStructures;
+            [Tooltip("The structures that the faction must not let be killed.")]
+            public List<Health> vitalStructures;
+        }
     }
 }
