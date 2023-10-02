2aa24ca8af3c88540995afd20cd5d23865e94384
Liam Healey <56327428+LiamDHealey@users.noreply.github.com>
2023-10-02 17:53:33 -0600

    Humans Amlost

---
+++ Assets/Source/Combat/Behavoirs/AttackShinesBehavior.cs
@@ -0,0 +1,85 @@
+using ElementalEngagement.Combat;
+using ElementalEngagement.Favor;
+using ElementalEngagement.Player;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+/// <summary>
+/// Cause this to move to shrines when not being commanded
+/// </summary>
+public class AttackShinesBehaviour : MonoBehaviour
+{
+    [Tooltip("The agent used to move this.")]
+    [SerializeField] private NavMeshAgent agent;
+
+    [Tooltip("The allegiance of this. Leave null for this to chase any object with a health component.")]
+    [SerializeField] private Allegiance allegiance;
+
+    [Tooltip("The distance away from the closet target to stop at.")][Min(0)]
+    [SerializeField] private float stoppingRange = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NavMesh.SamplePosition(transform.position, out NavMeshHit navMeshHit, 50f, 1);
+        transform.position = navMeshHit.position;
+
+        List<Transform> shrines = FindObjectsOfType<Spawner>()
+            .Where(spawner => spawner.GetComponent<Health>()    != null)
+            .Where(spawner => !spawner.GetComponent<Allegiance>().CheckFactionAllegiance(allegiance))
+            .Select(spawner => transform)
+            .ToList();
+
+        foreach (Transform shrine in shrines)
+        {
+            shrine.GetComponent<Health>().onDamaged.AddListener(delegate { shrines.Remove(shrine); });
+        }
+
+        // Waits until this is idle then moves to visible targets.
+        StartCoroutine(AttackWhenIdle());
+        IEnumerator AttackWhenIdle()
+        {
+            IEnumerable<CommandReceiver> receivers = GetComponents<CommandReceiver>();
+
+            while (true)
+            {
+                yield return null;
+
+                if (receivers.Any(r => r.commandInProgress))
+                    continue;
+
+                // Attacks any target in its original vision range until commanded.
+                // Will also move back to its initial position when no targets are visible.
+                yield return StartCoroutine(AttackTarget());
+                IEnumerator AttackTarget()
+                {
+                    while (!receivers.Any(r => r.commandInProgress) && shrines.Count > 0)
+                    {
+
+                        float SqrDistance(Transform target) => (target.position - transform.position).sqrMagnitude;
+
+                        // Get the closest target
+                        Transform closetsTarget = shrines
+                            .Aggregate((closest, next) =>
+                            {
+                                if (SqrDistance(closest) < SqrDistance(next))
+                                    return next;
+                                else
+                                    return closest;
+                            });
+
+                        agent.isStopped = SqrDistance(closetsTarget) < stoppingRange * stoppingRange;
+                        agent.SetDestination(closetsTarget.position);
+
+                        yield return null;
+                    }
+                }
+            }
+        }
+    }
+}
