f94a244cad9384c46868ffce048cf6fd1fd8d667
eharrison39 <u1358661@utah.edu>
2023-10-03 20:58:25 -0600

    Fixed Health Modification
    Health Modification now works.

--- Assets/Source/Combat/StatModifiers/HealthModificationArea.cs
+++ Assets/Source/Combat/StatModifiers/HealthModificationArea.cs
@@ -18,8 +18,10 @@ namespace ElementalEngagement.Combat
         {
             area.onTriggerEnter.AddListener(OnTriggerEnter);
             area.onTriggerExit.AddListener(OnTriggerExit);
-
-            void OnTriggerEnter(Collider collider)
+        }
+        private void OnDestroy()
+        {
+            foreach (Collider collider in area.overlappingColliders)
             {
                 Health health = collider.GetComponent<Health>();
 
@@ -27,46 +29,63 @@ namespace ElementalEngagement.Combat
 
                 // Check if health is null.
                 if (health.Equals(null) || all == null)
-                    return;
-
-                // If collider allegiance does not equal area of effect's allegiance, or the area of effect's allegiance is unaligned.
-                if (all.faction != allegiance.faction || allegiance.faction == Faction.Unaligned)
-                    return;
-
-                // If collider god is not equal to the area of effect's god, or the area of effect's god is unaligned.
-                if (all.god != allegiance.god || allegiance.god == Favor.MinorGod.Unaligned)
-                    return;
+                    continue;
 
                 float hpPercent = health.hp / health.maxHp;
 
-                health.maxHp *= deltaMaxHp;
+                health.maxHp -= deltaMaxHp;
 
                 health.hp = health.maxHp * hpPercent;
             }
+        }
 
-            void OnTriggerExit(Collider collider)
-            {
-                Allegiance all = collider.GetComponent<Allegiance>();
+        void OnTriggerEnter(Collider collider)
+        {
+            Health health = collider.GetComponent<Health>();
 
-                Health health = collider.GetComponent<Health>();
+            Allegiance all = collider.GetComponent<Allegiance>();
 
-                if (health == null || all == null)
-                    return;
+            // Check if health is null.
+            if (health.Equals(null) || all == null)
+                return;
 
-                // If collider allegiance does not equal area of effect's allegiance, or the area of effect's allegiance is unaligned.
-                if (all.faction != allegiance.faction || allegiance.faction == Faction.Unaligned)
-                    return;
+            //// If collider allegiance does not equal area of effect's allegiance.
+            //if (all.faction == allegiance.faction)
+            //    return;
 
-                // If collider god is not equal to the area of effect's god, or the area of effect's god is unaligned.
-                if (all.god != allegiance.god || allegiance.god == Favor.MinorGod.Unaligned)
-                    return;
+            //// If collider god is not equal to the area of effect's god, or the area of effect's god is unaligned.
+            //if (all.god != allegiance.god || allegiance.god == Favor.MinorGod.Unaligned)
+            //    return;
 
-                float hpPercent = health.hp / health.maxHp;
+            float hpPercent = health.hp / health.maxHp;
 
-                health.maxHp /= deltaMaxHp;
+            health.maxHp += (deltaMaxHp / 2);
 
-                health.hp = health.maxHp * hpPercent;
-            }
+            health.hp = health.maxHp * hpPercent;
+        }
+
+        void OnTriggerExit(Collider collider)
+        {
+            Allegiance all = collider.GetComponent<Allegiance>();
+
+            Health health = collider.GetComponent<Health>();
+
+            if (health == null || all == null)
+                return;
+
+            //// If collider allegiance does not equal area of effect's allegiance, or the area of effect's allegiance is unaligned.
+            //if (all.faction != allegiance.faction || allegiance.faction == Faction.Unaligned)
+            //    return;
+
+            //// If collider god is not equal to the area of effect's god, or the area of effect's god is unaligned.
+            //if (all.god != allegiance.god || allegiance.god == Favor.MinorGod.Unaligned)
+            //    return;
+
+            float hpPercent = health.hp / health.maxHp;
+
+            health.maxHp -= (deltaMaxHp / 2);
+
+            health.hp = health.maxHp * hpPercent;
         }
     }
 }
