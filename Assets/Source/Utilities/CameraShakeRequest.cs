b1b8a45e35ccb0e3db72f3d249495e0558d5087d
eharrison33 <u1358661@utah.edu>
2023-11-06 21:08:16 -0700

    Attempt to fix Camera Shake

--- Assets/Source/Utilities/CameraShakeRequest.cs
+++ Assets/Source/Utilities/CameraShakeRequest.cs
@@ -7,10 +7,28 @@ using UnityEngine;
 public class CameraShakeRequest : MonoBehaviour
 {
     public bool shouldShake = false;
+    public float elapsedTime = 5f;
+    public bool hasShaken = false;
+    
 
     void Update()
     {
-      CameraShake.isShaking = shouldShake;
+        CameraShake.isShaking = shouldShake;
+
+        if (shouldShake)
+            hasShaken = true;
+
+        if (hasShaken)
+            elapsedTime += Time.deltaTime;
+
+        if (elapsedTime > 5)
+            shouldShake = false;
+    }
+
+    void OnDestroy()
+    {
+        CameraShake.isShaking = false;
+        hasShaken = false;
     }
     //public bool isShaking   { set => CameraShake.isShaking = isTrue; }
 
