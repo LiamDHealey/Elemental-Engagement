5e3b725053d213b58a3f2621e1b2701dc311b53a
DylsterSM <thedylstersm@gmail.com>
2023-11-07 01:02:15 -0700

    Revert "Merge branch 'main' into Map-Integration2"
    This reverts commit 9c36976f6d12b43e6730a441709a1595a4ef1e19, reversing
    changes made to 55a6b7a7f79df8429375b688dc28c03c5984387d.

--- Assets/Source/Utilities/CameraShakeRequest.cs
+++ Assets/Source/Utilities/CameraShakeRequest.cs
@@ -7,28 +7,10 @@ using UnityEngine;
 public class CameraShakeRequest : MonoBehaviour
 {
     public bool shouldShake = false;
-    public float elapsedTime = 5f;
-    public bool hasShaken = false;
-    
 
     void Update()
     {
-        CameraShake.isShaking = shouldShake;
-
-        if (shouldShake)
-            hasShaken = true;
-
-        if (hasShaken)
-            elapsedTime += Time.deltaTime;
-
-        if (elapsedTime > 5)
-            shouldShake = false;
-    }
-
-    void OnDestroy()
-    {
-        CameraShake.isShaking = false;
-        hasShaken = false;
+      CameraShake.isShaking = shouldShake;
     }
     //public bool isShaking   { set => CameraShake.isShaking = isTrue; }
 
