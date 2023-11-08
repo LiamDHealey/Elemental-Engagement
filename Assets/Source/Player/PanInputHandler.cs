e156a49465c1ba0e20d4b6fd78a692ddfc8be04b
Christian Hartman <122404963+Is-This-One-Avaiable@users.noreply.github.com>
2023-11-07 21:45:54 -0700

    Worked on CameraBoundary

--- Assets/Source/Player/PanInputHandler.cs
+++ Assets/Source/Player/PanInputHandler.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 namespace ElementalEngagement.Player
 {
@@ -16,13 +17,22 @@ namespace ElementalEngagement.Player
         [Tooltip("The speed multiplier for panning.")]
         [SerializeField] private float panspeed = 1f;
 
+        [SerializeField] private Vector2 bounds;
+
+        [SerializeField] Vector3 startPos;
+
         /// <summary>
         /// Move this
         /// </summary>
         public void Pan(Vector2 input)
         {
             Vector2 delta = input * panspeed * Time.deltaTime;
-            transform.position += new Vector3(delta.x, 0, delta.y);
+
+            transform.position = new Vector3(
+                Mathf.Clamp(transform.position.x + delta.x, startPos.x - bounds.x, startPos.x + bounds.x), 
+                transform.position.y,
+                Mathf.Clamp(transform.position.z + delta.y, startPos.z - bounds.y, startPos.z + bounds.y)
+                );
         }
     }
 }
