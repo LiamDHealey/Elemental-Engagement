8975f751a781e327aa6906a3729b3c35f40828b8
Devin Fink <devinfink007@gmail.com>
2023-11-13 17:10:03 -0700

    Ear death issue is fixed

---
+++ Assets/DelayStart.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayStart : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(ActivateRoutine());
+    }
+
+    private IEnumerator ActivateRoutine()
+    {
+
+        // make a list of all children
+        Transform[] ChildrenTransforms = this.gameObject.GetComponentsInChildren<Transform>();
+        foreach (Transform t in ChildrenTransforms)
+            if (0 == t.childCount)
+                t.gameObject.SetActive(false); // disable the children
+
+        yield return new WaitForSeconds(2);
+
+        foreach (Transform t in ChildrenTransforms)
+            t.gameObject.SetActive(true);      // restore all the disabled objects in the list (even the parent)
+    }
+}
