90b834c518f00207d4c241f08ab46a0b03e96512
Christian Hartman <122404963+Is-This-One-Avaiable@users.noreply.github.com>
2023-11-24 12:08:35 -0700

    Added controllers for positive and negative boundaries

--- Assets/Source/Player/CameraMovementHandler.cs
+++ Assets/Source/Player/CameraMovementHandler.cs
@@ -24,13 +24,22 @@ namespace ElementalEngagement.Player
         [SerializeField] private float zoomspeed = 1f;
 
         [Tooltip("The camera boundary for the Grasslands map. Y-axis is for zoom height. The bigger the Y-axis, the farther one can zoom out.")]
-        [SerializeField] private Vector3 map1Bounds;
-        [Tooltip("The camera boundary for the Desert map. Y-axis is for zoom height. The bigger the Y-axis, the farther one can zoom out.")]
-        [SerializeField] private Vector3 map2Bounds;
-        [Tooltip("The camera boundary for the Tundra map. Y-axis is for zoom height. The bigger the Y-axis, the farther one can zoom out.")]
-        [SerializeField] private Vector3 map3Bounds;
+        [SerializeField] private Vector3 map1PositiveBounds;
+        [Tooltip("The camera boundary for the Grasslands map.")]
+        [SerializeField] private Vector3 map1NegativeBounds;
 
-        [SerializeField] private Vector3 bounds;
+        [Tooltip("The camera boundary for the Desert map. Y-axis is for zoom height. The bigger the Y-axis, the farther one can zoom out.")]
+        [SerializeField] private Vector3 map2PositiveBounds;
+        [Tooltip("The camera boundary for the Desert map.")]
+        [SerializeField] private Vector3 map2NegativeBounds;
+
+        [Tooltip("The camera boundary for the Tundra map. Y-axis is for zoom height. The bigger the Y-axis, the farther one can zoom out.")]
+        [SerializeField] private Vector3 map3PositiveBounds;
+        [Tooltip("The camera boundary for the Tundra map.")]
+        [SerializeField] private Vector3 map3NegativeBounds;
+
+        private Vector3 boundsPositive;
+        private Vector3 boundsNegative;
 
         [SerializeField] Vector3 startPos;
 
@@ -39,15 +48,18 @@ namespace ElementalEngagement.Player
             UnityEngine.SceneManagement.Scene currentMap = SceneManager.GetActiveScene();
             if (currentMap.name == "GrasslandMap")
             {
-                bounds = map1Bounds;
+                boundsPositive = map1PositiveBounds;
+                boundsNegative = map1NegativeBounds;
             }
             else if (currentMap.name == "DesertMap")
             {
-                bounds = map2Bounds;
+                boundsPositive = map2PositiveBounds;
+                boundsNegative = map2NegativeBounds;
             }
             else if (currentMap.name == "TundraMap")
             {
-                bounds = map3Bounds;
+                boundsPositive = map3PositiveBounds;
+                boundsNegative = map3NegativeBounds;
             }
         }
 
@@ -59,9 +71,9 @@ namespace ElementalEngagement.Player
             Vector2 delta = input * panspeed * Time.deltaTime;
 
             transform.position = new Vector3(
-                Mathf.Clamp(transform.position.x + delta.x, startPos.x - bounds.x, startPos.x + bounds.x), 
+                Mathf.Clamp(transform.position.x + delta.x, startPos.x - boundsNegative.x, startPos.x + boundsPositive.x), 
                 transform.position.y,
-                Mathf.Clamp(transform.position.z + delta.y, startPos.z - bounds.z, (startPos.z + bounds.z)-30)
+                Mathf.Clamp(transform.position.z + delta.y, startPos.z - boundsNegative.z, (startPos.z + boundsPositive.z)-30)
                 );
         }
 
@@ -74,7 +86,7 @@ namespace ElementalEngagement.Player
 
             transform.position = new Vector3(
                 transform.position.x,
-                Mathf.Clamp(transform.position.y + delta.y, startPos.y, startPos.y + bounds.y),
+                Mathf.Clamp(transform.position.y + delta.y, startPos.y, startPos.y + boundsPositive.y),
                 transform.position.z
                 );
         }
